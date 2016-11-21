4a9edfe843aaedaf480b90f6190907500c03decb
Jamal Moir <jamal.moir@gmail.com>
2016-11-21 18:48:45 +0000

    Update constructor parameters and add epsilon initialisation.

--- models/neural_network.py
+++ models/neural_network.py
@@ -5,15 +5,23 @@ import numpy as np
 class NeuralNetwork(model.Model):
     """Implements a neural network."""
 
-    def __init__(self, input_node_count, output_node_count, hlayer_count,
-                 hlayer_node_count, epsilon=0.0001):
-        self.input_node_count = input_node_count
-        self.ouput_node_count = output_node_count
+    def __init__(self, X, y, label_count, hlayer_count, hlayer_node_count):
+        self.X = X
+        self.y = y
+        self.m, self.input_count = X.shape
+        self.label_count = label_count
         self.hlayer_count = hlayer_count
         self.hlayer_node_count = hlayer_node_count
-        self.epsilon = 0.0001
 
+        self._init_epsilon()
         self._init_weights()
+
+    def _init_epsilon(self):
+        """Initilise the value of epsilon to be
+        sqrt(6)/(sqrt(input_nodes) + sqrt(output_nodes)).
+        """
+        self.epsilon = (np.sqrt(6)) / (np.sqrt(self.input_count) +
+                                       np.sqrt(self.label_count))
 
     def _init_weights(self):
         """Initialise weights of input layer and each hidden layer to random
