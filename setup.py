4ed54ad2cc25c0d3db745585b6e8261c731dd99e
Jamal Moir <jamal.moir@gmail.com>
2017-02-14 15:40:45 +0000

    Edit setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Jamal Moir',
     author_email='jamal@jamalmoir.com',
-    packages=['ml_components', 'mlcomponents.models'],
+    packages=['ml_components', 'ml_components.models', 'ml_components.models.utils'],
     license='LICENSE.txt',
     description='A Library of Maching Learning Models and Algorithms.',
     long_description=open('README.md').read(),
